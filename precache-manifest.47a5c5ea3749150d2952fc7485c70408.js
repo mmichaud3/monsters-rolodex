ad10f6d750a3775fb1aa3870f17f359edf24c4ec
Michael Michaud <mmichaud3@yahoo.com>
2020-04-22 13:39:20 -0700

    Updates

---
+++ precache-manifest.47a5c5ea3749150d2952fc7485c70408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894d9a8f7f3ff6cab5df2adaed70d0de",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "de4fd97465248369618b",
+    "url": "/monsters-rolodex/static/css/main.7c3adaed.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4fd97465248369618b",
+    "url": "/monsters-rolodex/static/js/main.eba9dcf2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
